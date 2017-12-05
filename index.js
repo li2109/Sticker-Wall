0a87aa691f5916991b321a4055242d6378b3f29e
Weizhi Li <li2109@purdue.edu>
2017-12-05 02:19:26 -0500

    Select color from the list, color change transition

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var vm = new Vue({
   el: "#app",
   data: {
+    colorList:[
+      {
+        name: "yellow",
+        color: "#FFEB67"
+      },{
+        name: "blue",
+        color: "#A5D8D6"
+      },{
+        name: "red",
+        color: "#EF898C"
+      },{
+        name: "green",
+        color: "#CBE196"
+      }
+    ],
     postits: [
       {
         text: "Something",
@@ -14,7 +29,8 @@ var vm = new Vue({
       return {
        left: p.pos.x+"px",
        top: p.pos.y+"px",
-        'font-size': (240-10)/(p.text.length) -10 + "px"
+        'font-size': (240-10)/(p.text.length) -10 + "px",
+        backgroundColor: this.colorList.find(o=>o.name==p.color).color
       };
     }
   }
