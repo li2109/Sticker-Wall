d5b278b3014e988fbe8452839f3a680a1cd3ba35
Weizhi Li <li2109@purdue.edu>
2017-12-05 02:50:32 -0500

    drag and drop the sticker; bug: onmouseup does not work right now.

--- index.js
+++ index.js
@@ -21,8 +21,13 @@ var vm = new Vue({
         text: "Something",
         color: "yellow",
         pos: { x: 20, y: 0 }
+      }, {
+        text: "Something",
+        color: "yellow",
+        pos: { x: 20, y: 400 }
       }
-    ]
+    ],
+    nowId : -1
   },
   methods: {
     postitCss(p) {
@@ -31,7 +36,24 @@ var vm = new Vue({
        top: p.pos.y+"px",
         'font-size': (240-10)/(p.text.length) -10 + "px",
         backgroundColor: this.colorList.find(o=>o.name==p.color).color
-      };
+      }
+    },
+    selectId(id){
+      console.log(id)
+      this.nowId =id
     }
   }
-});
+})
+
+window.onmousemove=function (evt){
+  console.log(evt)
+  if(vm.nowId != -1){
+      vm.postits[0].pos.x=evt.pageX
+      vm.postits[0].pos.y=evt.pageY
+  }
+
+}
+
+window.onmouseup = () =>{
+  vm.nowId = -1
+}
