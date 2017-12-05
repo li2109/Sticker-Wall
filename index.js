4c5a33abceb8d3ec5eca7305f1d3df532dec2947
Weizhi Li <li2109@purdue.edu>
2017-12-05 02:09:24 -0500

    Dynamic animation when input a sentence

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ var vm = new Vue({
         pos: { x: 20, y: 0 }
       }
     ]
+  },
+  methods: {
+    postitCss(p) {
+      return {
+       left: p.pos.x+"px",
+       top: p.pos.y+"px",
+        'font-size': (240-10)/(p.text.length) -10 + "px"
+      };
+    }
   }
 });
